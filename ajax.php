446cd3087e4ac8f4683d70b6294b60b8882430c2
Monty Anderson <montyanderson1@googlemail.com>
2014-09-14 16:04:42 +0100

    only shows errors if on localhost

--- ajax.php
+++ ajax.php
@@ -7,9 +7,11 @@ date_default_timezone_set("Etc/GMT-1");
 
 ob_start();
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
+if($_SERVER["HTTP_HOST"] == "localhost") {
+	ini_set('display_errors',1);
+	ini_set('display_startup_errors',1);
+	error_reporting(-1);
+}
 
 $mongo = new Mongo();
 
